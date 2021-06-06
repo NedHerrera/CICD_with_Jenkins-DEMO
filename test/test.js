0dbadf97a282026f45763e58389ef2164663e9e8
Nery Eduardo Herrera <tesco.herrera.edu@gmail.com>
2021-06-06 19:53:25 +0200

    Prueba

--- test/test.js
+++ test/test.js
@@ -7,7 +7,7 @@ var practica1 = require('../index');
 describe('Testing assert function: ', function() {
     describe('Check addTest Function', function(){
       it('Check the returned value using : assert.equal(value, value): ', function() {
-         result = practica1.validarnumeros(10,5)
+         result = practica1.validarnumeros(5,10)
          assert.equal(result, true);
       });
    });
